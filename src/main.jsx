10dfbc38eac54a5716bd9408966e56cc9091f10a
Avishka426 <pathumwghm@gmail.com>
2025-03-13 10:14:04 +0530

    Test link

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navbar from './Component/Navbar.jsx'
-// import About from './Component/About.jsx'
-// import Education from './Component/Education.jsx'
-// import Services from './Component/Services.jsx'
-// import Skills from './Component/Skills.jsx'
+import About from './Component/About.jsx'
+import Education from './Component/Education.jsx'
+import Services from './Component/Services.jsx'
+import Skills from './Component/Skills.jsx'
 import Contact from './Component/Contact.jsx'
-// import Footer from './Component/Footer.jsx'
+import Footer from './Component/Footer.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <Navbar />
+    <About />
+    <Education />
+    <Services />
+    <Skills />
     <Contact />
+    <Footer />
   </StrictMode>,
 )
