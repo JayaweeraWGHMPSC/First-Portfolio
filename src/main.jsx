92c7d97ae30a0ca5d29f7f45f9b409816c167bd1
Pathum <pathumwghm@gmail.com>
2025-09-21 17:25:02 +0530

    Loading

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AnimatedBackground from './Component/AnimatedBackground.jsx'
-import Navbar from './Component/Navbar.jsx'
-import About from './Component/About.jsx'
-import Education from './Component/Education.jsx'
-import Services from './Component/Services.jsx'
-import Skills from './Component/Skills.jsx'
-import Contact from './Component/Contact.jsx'
-import Footer from './Component/Footer.jsx'
-import Project from './Component/Project.jsx'
+import App from './App.jsx'
 
 console.log('Main.jsx is loading...')
 const rootElement = document.getElementById('root')
@@ -18,15 +10,7 @@ console.log('Root element:', rootElement)
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <AnimatedBackground />
-      <Navbar />
-      <About />
-      <Education />
-      <Services />
-      <Project />
-      <Skills />
-      <Contact />
-      <Footer />
+      <App />
     </StrictMode>,
   )
 } else {
